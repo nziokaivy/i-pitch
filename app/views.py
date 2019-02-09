60056091e1b2ae8bf040026171b28427647b2c56
nziokaivy <nziokaivy@gmail.com>
2019-02-09 23:55:05 +0300

    create models.py file

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
@@ -8,27 +10,7 @@ app.config['SECRET_KEY'] = '21ad0163f7774319409a266fcca95b16'
 app.config['SQLACHEMY_DATABASE_URI'] = 'sqlite:///site.db '
 
 db = SQLAlchemy(app)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    username = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-    password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship('Post', backref='author', lazy=True)
-
-    def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
-
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(20), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)
-    
-    def __repr__(self):
-        return f"Post('{self.title}', '{self.date_posted}')"
+migrate = Migrate(app, db)
 
 
 
