5ecbd7f055ee5e2cab785d593193390e52874c8c
nziokaivy <nziokaivy@gmail.com>
2019-02-11 12:52:00 +0300

    add reset request route

--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ import secrets
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, abort
 from app import app, db
-from app.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
+from app.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm, RequestResetForm, ResetPasswordForm
 from app.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -163,3 +163,10 @@ def user_posts(username):
         .paginate(page=page, per_page=5)
     title = 'User'
     return render_template('user_posts.html', title=title, posts=posts, user=user)    
+
+@app.route('/reset_password', methods=['GET', 'POST'])
+def reset_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+        form = RequestResetForm()
+        return render_template('reset_request.html', title='Reset Password', form=form)
