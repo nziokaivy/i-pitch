7fd71907ea5cc976510a0483e8bf1178e23b59e5
nziokaivy <nziokaivy@gmail.com>
2019-02-11 12:41:53 +0300

    add requestform class

--- app/forms.py
+++ app/forms.py
@@ -54,3 +54,7 @@ class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
     submit = SubmitField('Post')
+
+class RequestResetForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email() ])
+    submit = SubmitField('Request Password Reset')
--- app/models.py
+++ app/models.py
@@ -16,7 +16,8 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
     pass_secure = db.Column(db.String(255), nullable=False)
-
+    
+@staticmethod
 def get_reset_token(self, expires_sec=1800):
     s = Serializer(app.config['SECRET_KEY'], expires_sec)
     return s.dumps({'user_id' : self.id}).decode('utf-8')
